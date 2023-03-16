348f0953f0b7e72b3d70890431a307511881abba
sleaz13b <sleazieb@gmail.com>
2023-03-16 06:36:34 -0700

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,28 +1,11 @@
 #include <stdio.h>
-/*
- * main - entry point
- *
- * Description: using sizeof to print the various types
- *
- * Return: always(0) success
- */
-/*int main(void)
-{
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
-	*sizeof evaluates zize of variable
-	printf("Size of a char: %lu byte(s)\n" (unsigned long)sizeof (a));
-	printf("Size of an int: %lu byte(s)\n" (unsigned long)sizeof (b));
-	printf("Size of a long int: %lu byte(s)\n" (unsigned long)sizeof (c));
-	printf("Size of a long long int: %lu byte(s)\n" (unsigned long)sizeof (d));
-	printf("Size of a float: %lu byte(s)\n" (unsigned long)sizeof (e));
 
-	return (0);
-}
-*/
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ *
+ */
 int main(void)
 {
 		int a;
