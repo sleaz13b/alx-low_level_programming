d95e636c08da5372c16b5f15edcc475a5888b54a
sleaz13b <sleazieb@gmail.com>
2023-03-16 05:13:00 -0700

    commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <Mstdio.h>
+
+/*COMMENTS
+ * main - entry point
+ * description: a program that prints with put function
+ * return always (0) success
+ */
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle\");
+ return(0);
