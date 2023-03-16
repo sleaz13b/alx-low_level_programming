1f6def8973f40092d58527c003d4797e59df2933
sleaz13b <sleazieb@gmail.com>
2023-03-16 05:15:28 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
  * description: a program that prints with put function
  * return always (0) success
  */
-int main(void)
+int main(void);
 {puts("\"Programming is like building a multilingual puzzle\");
  return(0);
