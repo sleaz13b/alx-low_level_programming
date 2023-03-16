666c6eeae8fc8b8a91b910d602b1e1ee0d94d799
sleaz13b <sleazieb@gmail.com>
2023-03-16 05:33:59 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,6 +2,6 @@
 /*COMMENTS*/
 int main(void)
 {
-	printf(with proper grammar, but the outcome is a piece of art,);
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
