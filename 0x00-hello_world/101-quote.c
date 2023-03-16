587903edd9d4a90f7f54582eebb237767ea2d990
sleaz13b <sleazieb@gmail.com>
2023-03-16 07:05:40 -0700

    commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ * main - Entry Point
+ *
+ * Description: print a quote using write function
+ *    ssize_t write(int fd, const void *buf, size_t count);
+ * Return(1) success
+ */
+int main(void)
+{
+       char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+       write(1, quo, 59);
+return(1);
+}
